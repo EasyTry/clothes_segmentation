e1889d736c3293ca957d326b3b940451717b28a5
Lucy Airapetyan <52498629+Leggerla@users.noreply.github.com>
2021-06-01 23:37:17 +0300

    Update upload_checkpoint.py

--- upload_checkpoint.py
+++ upload_checkpoint.py
@@ -2,7 +2,7 @@ import gdown
 import os
 
 checkpoint_dir = './pretrained_weights/'
-os.makedirs(checkpoint_dir, exist_ok=True)
+os.makedirs(checkpoint_dir, exist_ok=False)
 model_restore = checkpoint_dir + 'inference.pth'
 url = 'https://drive.google.com/u/0/uc?id=1eUe18HoH05p0yFUd_sN6GXdTj82aW0m9'
 gdown.download(url, model_restore, quiet=False)
